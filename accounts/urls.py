afaa096a173a055be3e59abf550e8e413df9ec89
xero7689 <volleyp7689@gmail.com>
2025-07-07 22:41:21 +0800

    fix: Redirect user to account status view after successful login

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("register/", views.register, name="register"),
     path(
         "login/",
-        auth_views.LoginView.as_view(template_name="accounts/login.html"),
+        auth_views.LoginView.as_view(template_name="accounts/login.html", next_page="account_status"),
         name="login",
     ),
     path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
