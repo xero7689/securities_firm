67eb4a0a35d3a389208c82d7d23c74e2ad24f906
xero7689 <volleyp7689@gmail.com>
2025-07-07 21:56:54 +0800

    fix: Add unit-test for reviewer information

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -2,6 +2,10 @@ import pytest
 
 from accounts.models import Account
 from django.core.exceptions import ValidationError
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+
+User = get_user_model()
 
 
 @pytest.mark.django_db
@@ -48,3 +52,28 @@ def test_account_status_choices(user):
     account.additional_docs_reason = "Test additional docs reason"
     account.save()
     assert account.status == "additional_docs_required"
+
+
+@pytest.mark.django_db
+def test_reviewed_by_and_reviewed_at_fields(user):
+    reviewer = User.objects.create_user(
+        username="reviewer", email="reviewer@example.com", password="reviewpass123"
+    )
+    application = Account.objects.create(
+        user=user,
+        phone_number="123-456-7890",
+        address="123 Test St",
+        reviewed_by=reviewer,
+        reviewed_at=timezone.now(),
+    )
+    assert application.reviewed_by == reviewer
+    assert application.reviewed_at is not None
+
+
+@pytest.mark.django_db
+def test_reviewed_by_can_be_null(user):
+    application = Account.objects.create(
+        user=user, phone_number="123-456-7890", address="123 Test St"
+    )
+    assert application.reviewed_by is None
+    assert application.reviewed_at is None
